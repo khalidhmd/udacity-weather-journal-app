ba9f9df6115f084147a8a6c65b145b2fbcea72ee
khalidhmd <khalidhmd@outlook.com>
2019-09-03 21:31:13 +0200

    create basice post and get routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {
+
+};
 
 // Require Express to run server and routes
+const bodyParser = require('body-parser')
 const express = require('express')
 // Start up an instance of app
 const app = express()
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({
@@ -13,9 +17,25 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
-app.use(express.static('website'));
+// app.use(express.static('website'));
 
 
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(projectData))
+})
+
+app.post('/', (req, res) => {
+  projectData.temperature = req.body.temperature
+  projectData.date = req.body.date
+  projectData.userResponse = req.body.userResponse
+  console.log(projectData)
+  res.send(JSON.stringify(projectData))
+})
 // Setup Server
+const server = app.listen(8000, () => {
+  console.log('server is listening on port:', 8000)
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
+const APIKey = '49b9b809e566d9d0cb35f31930cdff2e'
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
